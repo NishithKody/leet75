60ef951739fedfb062faeae1d0718562290a4a16
NishithKody <113385563+NishithKody@users.noreply.github.com>
2023-02-08 02:07:09 -0500

    Create Isomorphic_strings.js
    https://leetcode.com/problems/isomorphic-strings/submissions/893872500/

---
+++ Isomorphic_strings.js
@@ -0,0 +1,35 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function(s, t) {
+
+    if(s.length !=t.length)
+    {
+        return false;
+    }
+
+    let map = new Map()
+    let set = new Set()
+
+    for(let i=0; i<s.length; i++){
+        
+        if(map.has(s[i])){
+            if(map.get(s[i])!=t[i]){
+                return false;
+            }
+        }   
+        else{
+            if(set.has(t[i])){
+                return false;
+            }
+            map.set(s[i],t[i])
+            set.add(t[i])
+        }
+        // console.log(map)
+
+    }
+    return true;
+    
+};
