a63bd2f88f0461d0987e747db5a3ccc1d00e8d34
NishithKody <113385563+NishithKody@users.noreply.github.com>
2023-02-10 14:59:04 -0500

    Update _longest_palindrome.ts

--- _longest_palindrome.ts
+++ _longest_palindrome.ts
@@ -32,3 +32,37 @@ function longestPalindrome(s: string): number {
 
 
 };
+
+
+//----------------attempt 2
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var longestPalindrome = function(s) {
+
+    let map = new Map()
+    let res = 0;
+    
+   for(let i of s){
+       map.set(i,map.get(i)+1||1)
+   }
+//    console.log(map)
+    let flag=0;
+   for(let i of map){
+       if(Math.floor(i[1]%2)==0){
+           res = res + i[1]
+       }
+       else{
+           flag=1;
+           res = res + i[1] -1
+       }
+   }
+   if(flag==1){
+       return res +1
+   }
+   else 
+    return res;
+    
+};
